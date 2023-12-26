1e952fcea0bbde2d2d5b7d904c2259800f79001b
Lyndon Purcell <ljpurcell.dev@gmail.com>
2023-12-27 07:22:40 +1100

    hashObject function. Still needs to write compressed contents

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const GOT_REPO = ".got"
+const READ_WRITE_PERM = 0700
 
 func main() {
-	Execute()
+	hashObject("main.go", true)
 }
 
 type Command struct {
@@ -16,7 +20,7 @@ type Command struct {
 	Short string
 	Long  string
 	Help  string
-	Run   func([]string) 
+	Run   func([]string)
 }
 
 func UnknownCommand(cmd string) *Command {
@@ -25,7 +29,7 @@ func UnknownCommand(cmd string) *Command {
 		Short: "Got did not recognise the subcommand",
 		Long:  "Got did not recognise the subcommand; try running \"got --help\" for more",
 		Run: func(s []string) {
-            exitWithError("Got did not recognise the subcommand %q", cmd)
+			exitWithError("Got did not recognise the subcommand %q", cmd)
 		},
 	}
 }
@@ -43,8 +47,8 @@ func InitCommand() *Command {
 				exitWithError("File named %q already exists", GOT_REPO)
 			}
 
-            os.Mkdir(GOT_REPO, 0700)
-            // TODO: Initialie with commit
+			os.Mkdir(GOT_REPO, READ_WRITE_PERM)
+			// TODO: Initialie with commit
 		},
 	}
 
@@ -61,14 +65,52 @@ func Execute() {
 	switch subCmd {
 	case "init":
 		cmd = InitCommand()
-    default:
-        cmd = UnknownCommand(subCmd)
+	default:
+		cmd = UnknownCommand(subCmd)
 	}
 
 	cmd.Run(os.Args[1:])
 }
 
-// utils
+// got library
+func hashObject(obj string, write bool) string {
+	if !exists(obj) {
+		exitWithError("Cannot hash %q. Object doesn't exist", obj)
+	}
+
+	objType := "blob"
+
+	if isDir(obj) {
+		objType = "tree"
+	}
+
+	info, err := os.Stat(obj)
+	if err != nil {
+		exitWithError("Could not get file size for hash of %q", obj)
+	}
+
+	toHash := fmt.Sprintf("%v %d\u0000", objType, info.Size())
+	hasher := sha1.New()
+	hasher.Write(hasher.Sum([]byte(toHash)))
+	id := hex.EncodeToString(hasher.Sum(nil))
+
+	if write {
+		objDir := filepath.Join(GOT_REPO, "objects", id[:2])
+		objFile := filepath.Join(objDir, id[2:])
+
+		os.MkdirAll(objDir, READ_WRITE_PERM)
+		file, err := os.Create(objFile)
+		if err != nil {
+			exitWithError("Could not write object %q using name %q in directory %q", obj, objFile, objDir)
+		}
+
+		defer file.Close()
+	}
+
+	return id
+}
+
+// general utils
 func exists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
