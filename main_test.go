0fa208aed9ac922066d4934f4405d1c050058a6c
Lyndon Purcell <ljpurcell.dev@gmail.com>
2023-12-28 17:05:42 +1100

    another test for hash blob function

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
@@ -43,16 +46,51 @@ func TestHashBlobCreatesObjectFile(t *testing.T) {
 	file.Write(text)
 	id := hashBlob(file.Name(), true)
 
-    blobDir := filepath.Join(GOT_REPO, "objects", id[:2])
-    blobFile := filepath.Join(blobDir, id[2:])
+	blobDir := filepath.Join(GOT_REPO, "objects", id[:2])
+	blobFile := filepath.Join(blobDir, id[2:])
 
-    if !exists(blobDir) || !isDir(blobDir) {
-        t.Errorf("No directory for blob %v at %v", id, blobDir)
+	if !exists(blobDir) || !isDir(blobDir) {
+		t.Errorf("No directory for blob %v at %v", id, blobDir)
+	}
+
+	if !exists(blobFile) && !isDir(blobDir) {
+		t.Errorf("No file for blob %v at %v in %v", id, blobFile, blobDir)
+	}
+}
+
+func TestHashBlobCompressesFileContents(t *testing.T) {
+	file, err := os.CreateTemp(".", "test_file_for_hash_blob")
+	if err != nil {
+		t.Fatalf("Could not create temp file: %v", err)
+	}
+	defer cleanUpTempFile(file)
+
+	text := "These are some bytes to be written"
+	file.Write([]byte(text))
+	id := hashBlob(file.Name(), true)
+
+    blobFile := filepath.Join(GOT_REPO, "objects", id[:2], id[2:])
+
+    fileContents, err := os.ReadFile(blobFile)
+    if err != nil {
+        t.Fatalf("Could not read contents from %v for decompression", blobFile)
     }
 
-    if !exists(blobFile) && !isDir(blobDir) {
-        t.Errorf("No file for blob %v at %v in %v", id, blobFile, blobDir)
+    data := bytes.NewReader(fileContents)
+    dec, err := zlib.NewReader(data)
+    if err != nil {
+        t.Fatalf("Could not create ZLIB reader: %v", err)
     }
+    dec.Close()
+
+    out, err := io.ReadAll(dec)
+
+    result := string(out)
+
+    if result != text {
+        t.Fatalf("Exp: %v. Actual: %v", text, result)
+    }
+
 }
 
 func cleanUpTempFile(file *os.File) {
