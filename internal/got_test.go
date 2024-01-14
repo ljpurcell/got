23f4dded3129570a6f352e3d0d2f7a5e3b6d705b
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-01-14 11:16:55 +1100

    test for Index

--- internal/got_test.go
+++ internal/got_test.go
@@ -16,18 +16,14 @@ import (
 	"github.com/ljpurcell/got/internal/utils"
 )
 
-func TestHashObjectForBlob(t *testing.T) {
-	file, err := os.CreateTemp(".", "test_file_for_hash_blob")
-	if err != nil {
-		t.Fatalf("Could not create temp file: %v", err)
-	}
-	defer cleanUpTempFile(file)
+const text string = "These are some bytes to be written"
 
-	text := []byte("These are some bytes to be written")
-	file.Write(text)
+func TestHashObjectForBlob(t *testing.T) {
+	file, err := createTempFileWithText(text)
+	defer cleanUpTempFile(file)
 	info, err := os.Stat(file.Name())
 
-	toHash := fmt.Sprintf("blob %d\u0000", info.Size())
+	toHash := fmt.Sprintf("blob %d\u0000%v", info.Size(), text)
 	hash := sha1.New()
 	hash.Write([]byte(toHash))
 	expectedId := hex.EncodeToString(hash.Sum(nil))
@@ -54,7 +50,7 @@ func TestHashObjectForBlob(t *testing.T) {
 		t.Errorf("No file for blob %v at %v in %v", id, blobFile, blobDir)
 	}
 
-	fileContents, err := os.ReadFile(blobFile)
+    fileContents, err := os.ReadFile(blobFile)
 	if err != nil {
 		t.Fatalf("Could not read contents from %v for decompression", blobFile)
 	}
@@ -71,6 +67,64 @@ func TestHashObjectForBlob(t *testing.T) {
 	if string(out) != string(text) {
 		t.Errorf("Exp: %v. Actual: %v", string(text), string(out))
 	}
+}
+
+func TestIndex(t *testing.T) {
+	index := got.Index{}
+
+	if index.Length() != 0 {
+		t.Errorf("Index length should be zero")
+	}
+
+	file, err := createTempFileWithText(text)
+	if err != nil {
+		t.Fatalf("Could not create temp tile in TestIndex: %v", err)
+	}
+
+	index.UpdateOrAddFromFile(file.Name())
+
+	if index.Length() != 1 {
+		t.Errorf("Index length should be one")
+	}
+
+	file.WriteString("This is some different text")
+
+	index.UpdateOrAddFromFile(file.Name())
+
+	if index.Length() != 1 {
+		t.Errorf("Index length should be one")
+	}
+
+	oldName, newName := file.Name(), "NEW_NAME"
+	if err := os.Rename(oldName, newName); err != nil {
+		t.Fatalf("Could not rename %q to %q: %v", file.Name(), "NEW_NAME", err)
+	}
+
+	index.UpdateOrAddFromFile(newName)
+
+	if index.Length() != 2 {
+		t.Errorf("Index length should be two")
+	}
+
+	index.RemoveFile(oldName)
+
+	if index.Length() != 1 {
+		t.Errorf("Index length should be two")
+	}
+
+
+}
+
+func createTempFileWithText(text string) (*os.File, error) {
+	file, err := os.CreateTemp(".", "temp_file_for_testing")
+	if err != nil {
+		return nil, fmt.Errorf("Could not create temp file: %v", err)
+	}
+
+	data := []byte(text)
+	file.Write(data)
+
+	return file, nil
 }
 
 func cleanUpTempFile(file *os.File) {
