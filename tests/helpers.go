f66363a7bb7176d53e4dee0ef5653c98feb80b86
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-08-24 17:01:51 +1000

    test helper for initialising testing repo

--- tests/helpers.go
+++ tests/helpers.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func getTestDataDirectory(t *testing.T) (path string, err error) {
+func GetTestDataDirectory(t *testing.T) (path string, err error) {
 	t.Helper()
 	dir, err := os.Getwd()
 	if err != nil {
@@ -15,4 +15,19 @@ func getTestDataDirectory(t *testing.T) (path string, err error) {
 	}
 
 	return filepath.Join(filepath.Dir(dir), "testdata"), nil
+}
+
+func InitialiseRepoInTestDirectory(t *testing.T) (repo string, cleanup func(), err error) {
+	t.Helper()
+	td, err := GetTestDataDirectory(t)
+	if err != nil {
+		t.Fatalf("can not intialise repo in test directory: %s", err)
+	}
+
+	repo = filepath.Join(td, ".got")
+
+	return repo, func() {
+		err = os.RemoveAll(repo)
+		t.Fatalf("could not remove repo in test directory: %s", err)
+	}, nil
 }
